418df4467cdec6d0cb6e9f45a1ea8374748a1072
supermegablockhead <supermegablockhead@gmail.com>
2026-05-14 13:36:16 +0200

    Test

---
+++ family_tree.c
@@ -0,0 +1,33 @@
+/*
+===================================================================================================================================================
+Fundamentals of Programming II: Trees and Graphs
+Team: Juanma Machado, Javier Olivé
+===================================================================================================================================================
+*/
+
+/*
+___________________________________________________________________________________________________________________________________________________
+Imports & Utility Functions
+___________________________________________________________________________________________________________________________________________________
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "small.h"
+#include "medium.h"
+#include "large.h"
+
+/* 
+CAN WE REDUCE .h 's TO NOT REPEAT THE SAME CODE?
+*/
+
+// Define as a command argument if we use large, medium or small, setting small as standard. Set root?
+
+// Road map computation
+// Bidirected graph
+
+// Route computation (cost, heuristics)
+
+// Family tree computation & print, BFS&DFS
+
+// Main
